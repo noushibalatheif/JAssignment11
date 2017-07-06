b9e86533e3ff8e592dae51bcb231beb4cfb5195e
noushibalatheif <noushibalatheif@gmail.com>
2017-07-06 10:38:56 -0700

    Java Assignment 11

---
+++ main.java
@@ -0,0 +1,14 @@
+package assignment11;
+
+public class main
+{
+	public static void main(String[] args)
+	{
+		permanentEmployee emp=new permanentEmployee("Jhon", 35, 1001,
+				"Male", 98967623, 2500,2500, 20000);
+		emp.setpcode("PM100");
+		emp.display();
+		emp.disp();
+	}
+
+}
---
+++ permanentEmployee.java
@@ -0,0 +1,30 @@
+package assignment11;
+
+public class permanentEmployee extends employee
+{
+
+	public permanentEmployee(String name, int age, int empcode, String gender,
+			int phno,int pf,int gratuity,int salary)
+	{
+		super(name, age, empcode, gender, phno, pf, gratuity, salary);
+	}
+
+	String permanentCode;
+
+	public void setpcode(String pcode)
+	{
+		this.permanentCode=pcode;
+	}
+	
+	public String getpcode()
+	{
+		return  permanentCode;
+	}
+	
+	void disp()
+	{
+		System.out.println("PermanentCode : "+ permanentCode);
+	}
+
+
+}
